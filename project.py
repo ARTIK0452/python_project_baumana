ec7a4e19e71ba984a3c2925cae8a173656bc11f9
ARTIK0452 <43519433+ARTIK0452@users.noreply.github.com>
2020-05-22 19:33:19 +0300

    Update project.py

--- project.py
+++ project.py
@@ -1,39 +1,32 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.label import Label
 
-from vk_api import vk_api
 
-class vk():
-	user = input('number phone: ')
-	passw = input('password: ')
-	def __init__(self, user, passw):
-		self.vk_session = vk_api.VkApi(str(user), str(passw))
-		self.vk_session.auth()
-		self,vk = vk_session.get_api()
-		print('userID: ', vk_session.token['user_id'])
+#from vk_api import vk_api
 	
+class EnterAccountWindow(Screen):
+	pass
+	#self.user = input('number phone: ')
+	#self.passw = input('password: ')
+	#def __init__(self, user, passw):
+		#vk_session = vk_api.VkApi(str(user), str(passw))
+		#vk_session.auth()
+		#vk = vk_session.get_api()
+		#print('userID: ', vk_session.token['user_id'])
+
 class MainWindow(Screen):
 	pass
 
-class SecondWindow(Screen):
-    pass
-
 class WindowManager(ScreenManager):
-    pass
+	pass
 
-kv = Builder.load_file('prog.kv')
+kv = Builder.load_file("login.kv")
 
 class MyProjectApp(App):
-    def build(self):
-        return kv
-	
-        
-        
+	def build(self):
+		return kv
+	       
 MyProjectApp().run()
-
 
