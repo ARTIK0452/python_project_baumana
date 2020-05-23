1edb860da7644bca8a57751aa454b4c759b3ae2d
ARTIK0452 <43519433+ARTIK0452@users.noreply.github.com>
2020-05-23 17:08:26 +0300

    Update project.py

--- project.py
+++ project.py
@@ -1,32 +1,32 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.label import Label
 
-
-#from vk_api import vk_api
-	
-class EnterAccountWindow(Screen):
-	pass
-	#self.user = input('number phone: ')
-	#self.passw = input('password: ')
-	#def __init__(self, user, passw):
-		#vk_session = vk_api.VkApi(str(user), str(passw))
-		#vk_session.auth()
-		#vk = vk_session.get_api()
-		#print('userID: ', vk_session.token['user_id'])
+#import sqlite3
+#connect = sqlite3.connect('users.db')
+#cursor = connect.cursor()
+#cursor.execute('CREATE TABLE IF NOT EXISTS users (login text) ')
+#connect.commit()
+#connect.close()
 
 class MainWindow(Screen):
-	pass
+    pass
+
+class WeatherWindow(Screen):
+    pass
+
 
 class WindowManager(ScreenManager):
-	pass
+    pass
 
-kv = Builder.load_file("login.kv")
 
-class MyProjectApp(App):
-	def build(self):
-		return kv
-	       
-MyProjectApp().run()
+kv = Builder.load_file("one.kv")
+
+
+class MyMainApp(App):
+    def build(self):
+        return kv
+
+
+MyMainApp().run()
 
