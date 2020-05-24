30c495bc8df2bdceb626dcd1831e3cf16cb21b78
ARTIK0452 <43519433+ARTIK0452@users.noreply.github.com>
2020-05-24 17:58:42 +0300

    Update project.py

--- project.py
+++ project.py
@@ -3,25 +3,18 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 import requests
 
-#import sqlite3
-#connect = sqlite3.connect('users.db')
-#cursor = connect.cursor()
-#cursor.execute('CREATE TABLE IF NOT EXISTS users (login text) ')
-#connect.commit()
-#connect.close()
-
 class MainWindow(Screen):
     pass
 
 class WeatherWindow(Screen):
-    def search_info_weather(self, requests):
+    def search_info_weather(self, cct):
         listKey = ['weather']
-        s_city = self.cityCountry.text
-        appid = "5d9d9f7178756dbc78d68958da985841"
+        #self.s_city = self.cityCountry.text
+        self.appid = "5d9d9f7178756dbc78d68958da985841"
         if self.search.on_release:
             try:
                 res = requests.get("http://api.openweathermap.org/data/2.5/find",
-                    params={'q': self.s_city, 'APPID': appid})
+                    params={'q': cct, 'APPID': self.appid})
                 data = res.json()
                 for k, v in data['list'][1].items():
                     print(k, v)
